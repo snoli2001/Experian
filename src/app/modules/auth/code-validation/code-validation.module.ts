5ad70d415189120bc508030bd4f9c43444b68506
snoli2001 <sebastian.noli@gmail.com>
2022-11-24 18:54:25 -0500

    hotfix

--- src/app/modules/auth/code-validation/code-validation.module.ts
+++ src/app/modules/auth/code-validation/code-validation.module.ts
@@ -1,10 +1,8 @@
-import { HttpClientModule } from '@angular/common/http';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { codeValidationRoutes } from './code-validation.routing';
 import { SharedModule } from 'app/shared/shared.module';
@@ -15,7 +13,6 @@ import { FuseAlertModule } from '@fuse/components/alert';
 @NgModule({
     declarations: [CodeValidationComponent],
     imports: [
-        CommonModule,
         RouterModule.forChild(codeValidationRoutes),
         MatFormFieldModule,
         MatInputModule,
